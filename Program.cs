a919dcd6c8cb75ad46b80c8d949f14b0d2043d30
juanst07 <001068289@senaimgaluno.com.br>
2025-04-24 14:04:47 -0300

    arquivos

---
+++ Program.cs
@@ -0,0 +1,14 @@
+var builder = WebApplication.CreateBuilder(args);
+var app = builder.Build();
+
+app.MapGet("/", () => "Hello World!");
+
+app.UseDefaultFiles(); // Serve index.html por padrão
+app.UseStaticFiles();  // Permite servir arquivos da pasta wwwroot
+
+app.MapGet("/sobre", async context =>
+{
+    await context.Response.SendFileAsync("wwwroot/sobre.html");
+});
+
+app.Run();
